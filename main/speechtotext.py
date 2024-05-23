00fc98322a551b435cdcdf66d22b613a994ffe04
anonimation <torterolatomas16@gmail.com>
2024-05-23 18:35:54 -0400

    basic file structure

---
+++ main/speechtotext.py
@@ -0,0 +1,3 @@
+# Simply need a way to convert from spoken audio to written speech
+# Handle errors like no understandable audio
+# Audio cleanup for better detection
---
+++ main/texttospeech.py
@@ -0,0 +1,1 @@
+# Read a problem to the user
